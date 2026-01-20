5bf6a8b325d600ccc2ea8f784efabaef234e9c15
Warren Crossing <warren@itcl.com.au>
2026-01-20 19:36:39 +1100

    delete old features

--- jss7-feature-pack/src/main/packages/org.mobicents.ss7.tools/content/bin/ss7-shell.sh
+++
@@ -1,1 +0,0 @@
-java -Dss7.connect -Dss7.username=admin -Dss7.password=admin -jar /media/work/.m2/repository/org/mobicents/protocols/ss7/management/shell-client/9.0.0-SNAPSHOT/shell-client-9.0.0-SNAPSHOT-shade.jar 
